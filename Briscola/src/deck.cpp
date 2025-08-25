7d8f6c76d91a06ea14063f993936146cb576d32d
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-08-25 19:09:39 +0300

    changed std::random_suffle to std::shuffle

--- Briscola/src/deck.cpp
+++ Briscola/src/deck.cpp
@@ -22,8 +22,11 @@ Deck::Deck() {
 }
 
 void Deck::shuffle() {
-    std::srand(unsigned(std::time(nullptr)));
-    std::random_shuffle(cards.begin(), cards.end());
+    //std::srand(unsigned(std::time(nullptr)));
+    //std::random_shuffle(cards.begin(), cards.end());
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(cards.begin(), cards.end(), g);
 
     // Ultima carta è la briscola (non rimossa dal mazzo)
     briscola = cards.back();
